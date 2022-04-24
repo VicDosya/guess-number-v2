46a2f7acdce544875f963f33293dc076463e4000
dosya <vicdosya@gmail.com>
2022-04-24 18:21:22 +0300

    Change route name, Generate new number on win, console log improved.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const paraGame = document.getElementById('paraGame');
 const inputGame = document.getElementById('inputGame');
 const gameButton = document.getElementById('gameButton');
@@ -7,10 +9,11 @@ const triesText = document.getElementById('triesText');
 
 //Send the server the client's guess//
 const sendInputGuess = async (guessNumber) => {
-    const response = await fetch("/ping/" + guessNumber);
+    const response = await fetch("/guess/" + guessNumber);
     const result = await response.json();
     resultText.innerText = result.message;
 };
+
 gameButton.addEventListener("click", () => {
     sendInputGuess(inputGame.value);
 });
